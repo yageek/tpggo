69cbfc6da5a80d577e8da1efa2c797b6eaf9c6d1
Yannick Heinrich <yannick.heinrich@gmail.com>
2018-12-14 08:32:56 +0100

    Improve test for url serialization
    Fixes #1

--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package tpggo
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,11 +11,11 @@ func TestURLSerialization(t *testing.T) {
 
 	fakeKey := "foo"
 	client := NewClient(fakeKey)
-	expected := "https://prod.ivtr-od.tpg.ch/v1/GetDisruptions.json?key=foo"
+	expected := fmt.Sprintf("https://prod.ivtr-od.tpg.ch/v1/GetDisruptions.json?key=%s", fakeKey)
 	URL := client.apiURL(getDisruptionsAPIPath, map[string]string{})
 	assert.Equal(t, expected, URL.String(), "The formatted url does not have the exepected values")
 
-	expected = "https://prod.ivtr-od.tpg.ch/v1/GetDisruptions.json?key=foo&key1=value1"
+	expected = fmt.Sprintf("https://prod.ivtr-od.tpg.ch/v1/GetDisruptions.json?key=%s&key1=value1", fakeKey)
 	URL = client.apiURL(getDisruptionsAPIPath, map[string]string{"key1": "value1"})
 	assert.Equal(t, expected, URL.String(), "The formatted url does not have the exepected values")
 }
